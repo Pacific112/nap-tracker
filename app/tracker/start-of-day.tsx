16e47ee07f775a01e6a9fb9f213ae0448b5f8bb9
maciej.koziara <maciej.koziara@playboycontractors.com>
2024-06-24 22:10:52 +0200

    [chore] Upgrade to next 15 and react 19

--- app/tracker/start-of-day.tsx
+++ app/tracker/start-of-day.tsx
@@ -1,18 +1,17 @@
 import { format } from "date-fns";
 import { LabeledCircle } from "@/app/tracker/labeled-circle";
-import React from "react";
+import React, { PropsWithoutRef, PropsWithRef } from "react";
 
 type Props = {
   date: Date;
   onClick?: () => void;
+  ref?: React.Ref<SVGGElement>;
 };
 
-export const StartOfDay = React.forwardRef<SVGGElement, Props>(
-  ({ date, onClick }, ref) => (
-    <LabeledCircle ref={ref} startingPoint={0} onClick={onClick}>
-      {format(date, "HH:mm")}
-    </LabeledCircle>
-  ),
+export const StartOfDay = ({ date, onClick, ref }: Props) => (
+  <LabeledCircle ref={ref} startingPoint={0} onClick={onClick}>
+    {format(date, "HH:mm")}
+  </LabeledCircle>
 );
 
 StartOfDay.displayName = "StartOfDay";
